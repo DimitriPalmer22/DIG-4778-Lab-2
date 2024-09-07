2777b575137df6f05c9faab062e933702fc368f2
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-09-06 23:55:31 -0400

    Select All Cubes Working

--- Assets/_Scripts/CubeScript.cs
+++ Assets/_Scripts/CubeScript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public float size = 1;
+}
 
-    // Update is called once per frame
-    void Update()
+
+[CustomEditor(typeof(CubeScript))] // Allows this editor to be used with the CubeScript class
+[CanEditMultipleObjects] // Allow multiple objects to be edited at once
+public class CubeEditor : Editor
+{
+    public override void OnInspectorGUI()
     {
-        
+        // Create a button to select all cubes
+        if (GUILayout.Button("Select All Cubes"))
+        {
+            var cubeScripts =
+                FindObjectsOfType<CubeScript>() // Get all game objects in the scene with a CubeScript component
+                    .Select(n => n.gameObject) // Select the game object attached to the CubeScript component
+                    .ToArray(); // Convert the IEnumerable to an array
+
+            // Select all the cubes
+            Selection.objects = cubeScripts;
+        }
     }
 }
--- Assets/_Scripts/SphereScript.cs
+++ Assets/_Scripts/SphereScript.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SphereScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] public float radius = 1;
 }
