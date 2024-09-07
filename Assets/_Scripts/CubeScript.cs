af7a212bd247d080332e5f4131d2f37989ac45e7
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-09-07 00:10:15 -0400

    Enable / Disable All

--- Assets/_Scripts/CubeScript.cs
+++ Assets/_Scripts/CubeScript.cs
@@ -16,23 +16,55 @@ public class CubeEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        // Make the buttons appear side by side
+        EditorGUILayout.BeginHorizontal();
+
         // Create a button to select all cubes
         if (GUILayout.Button("Select All Cubes"))
         {
             var cubeScripts =
-                FindObjectsOfType<CubeScript>() // Get all game objects in the scene with a CubeScript component
+                FindObjectsOfType<CubeScript>(true) // Get all game objects in the scene with a CubeScript component
                     .Select(n => n.gameObject) // Select the game object attached to the CubeScript component
                     .ToArray(); // Convert the IEnumerable to an array
 
             // Select all the cubes
             Selection.objects = cubeScripts;
         }
-        
+
         // Create another button to clear the selection
         if (GUILayout.Button("Clear Selection"))
         {
             // Reset the selection to an empty array
             Selection.objects = new Object[0];
         }
+
+        // End the horizontal layout
+        EditorGUILayout.EndHorizontal();
+
+        // Temporarily store the current background color so we can restore it later
+        var originalBackgroundColor = GUI.backgroundColor;
+
+        // Set the background color to red if the cube is active, otherwise set it to green
+        if ((target as CubeScript).gameObject.activeSelf)
+            GUI.backgroundColor = Color.red;
+        // Set the background color to green
+        else
+            GUI.backgroundColor = Color.green;
+        
+        // Create a button to enable / disable all cubes
+        if (GUILayout.Button("Disable / Enable all cubes", GUILayout.Height(40)))
+        {
+            var cubeScripts = FindObjectsOfType<CubeScript>(true); // Get all game objects in the scene with a CubeScript component
+
+            // Loop through each cube script
+            foreach (var cubeScript in cubeScripts)
+            {
+                // Toggle the enabled state of the cube
+                cubeScript.gameObject.SetActive(!cubeScript.gameObject.activeSelf);
+            }
+        }
+        
+        // Reset the background color to the original color
+        GUI.backgroundColor = originalBackgroundColor;
     }
 }
--- Assets/_Scripts/SphereScript.cs
+++ Assets/_Scripts/SphereScript.cs
@@ -15,11 +15,14 @@ public class SphereEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        // Make the buttons appear side by side
+        EditorGUILayout.BeginHorizontal();
+
         // Create a button to select all cubes
         if (GUILayout.Button("Select All Spheres"))
         {
             var sphereScripts =
-                FindObjectsOfType<SphereScript>() // Get all game objects in the scene with a SphereScript component
+                FindObjectsOfType<SphereScript>(true) // Get all game objects in the scene with a SphereScript component
                     .Select(n => n.gameObject) // Select the game object attached to the SphereScript component
                     .ToArray(); // Convert the IEnumerable to an array
 
@@ -33,5 +36,34 @@ public class SphereEditor : Editor
             // Reset the selection to an empty array
             Selection.objects = new Object[0];
         }
+        
+        // End the horizontal layout
+        EditorGUILayout.EndHorizontal();
+        
+        // Temporarily store the current background color so we can restore it later
+        var originalBackgroundColor = GUI.backgroundColor;
+
+        // Set the background color to red if the cube is active, otherwise set it to green
+        if ((target as SphereScript).gameObject.activeSelf)
+            GUI.backgroundColor = Color.red;
+        // Set the background color to green
+        else
+            GUI.backgroundColor = Color.green;
+        
+        // Create a button to enable / disable all cubes
+        if (GUILayout.Button("Disable / Enable all spheres", GUILayout.Height(40)))
+        {
+            var sphereScripts = FindObjectsOfType<SphereScript>(true); // Get all game objects in the scene with a SphereScript component
+
+            // Loop through each cube script
+            foreach (var sphereScript in sphereScripts)
+            {
+                // Toggle the enabled state of the cube
+                sphereScript.gameObject.SetActive(!sphereScript.gameObject.activeSelf);
+            }
+        }
+        
+        // Reset the background color to the original color
+        GUI.backgroundColor = originalBackgroundColor;
     }
 }
